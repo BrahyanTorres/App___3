058c8181c67a0d80ac2aa99f18ce80a7ae06722c
BrahyanTorres <143438365+BrahyanTorres@users.noreply.github.com>
2023-10-02 16:01:09 -0500

    Segundos Botones

--- app/src/main/java/com/example/app_3/MainActivity.java
+++ app/src/main/java/com/example/app_3/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.app_3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    Button goo, llama, alar;
+    Button goo, llama, alar, cam, ytb;
 
 
     @Override
@@ -20,9 +23,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        goo = findViewById(R.id.goole);
-        llama = findViewById(R.id.llama);
-        alar = findViewById(R.id.alarm);
+        goo = findViewById(R.id.btnGoole);
+        llama = findViewById(R.id.btnLlama);
+        alar = findViewById(R.id.btnAlarm);
+        cam = findViewById(R.id.btnCamara);
+        ytb = findViewById(R.id.btnYoutube);
 
         goo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +38,18 @@ public class MainActivity extends AppCompatActivity {
 
 
             }
+        });
+        llama.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent llamar = new Intent(Intent.ACTION_CALL, Uri.parse("3004988487"));
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)!=
+                        PackageManager.PERMISSION_GRANTED)
+                    return;
+                    startActivity(llamar);
+                }
+
+
         });
 
        alar.setOnClickListener(new View.OnClickListener() {
@@ -47,9 +64,30 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent abrirCamara= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (abrirCamara.resolveActivity(getPackageManager()) != null){
+                    startActivity(abrirCamara);
+                }
+
+
+            }
+        });
+
+        ytb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent irYoutube = new Intent(Intent.ACTION_VIEW);
+                irYoutube.setData(Uri.parse("http://www.youtube.com"));
+                startActivity(irYoutube);
+
+            }
+        });
+
 
     }
-
 }
 
 
